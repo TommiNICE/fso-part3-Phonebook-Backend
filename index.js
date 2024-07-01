ec9a06f95d006e1560e930decda94052c4cce2b1
tomi <tomislav.varga@icloud.com>
2024-07-01 09:42:54 +0200

    solution exercise 3.8

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
