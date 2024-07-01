f5edecfb81f70133289bfda4e9b3e754dd457e5e
tomi <tomislav.varga@icloud.com>
2024-07-01 09:12:57 +0200

    solution exercises 3.5 and 3.6

--- index.js
+++ index.js
@@ -55,6 +55,42 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
+const generateId = () => {
+    return String(Math.floor(Math.random() * 1000000))
+}
+
+app.post('/api/persons/', (req, res) => {
+    const body = req.body
+    const personExists = persons.some(person => person.name === body.name)
+    if (personExists) {
+      res.status(400).json({ error: 'name must be unique' })
+    } else {
+      
+
+      if (!body.name) {
+        return res.status(400).json({ 
+          error: 'name missing' 
+        })
+      }
+
+      if (!body.number) {
+        return res.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+
+      const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number, 
+      }
+
+      persons = persons.concat(person)
+
+      res.json(person)
+    }
+})
+
 const PORT = 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
