2e6b61f1e4ae76255230ff6ca956825992d3f40b
tomi <tomislav.varga@icloud.com>
2024-07-09 10:50:32 +0200

    solution exercise 3.17

--- index.js
+++ index.js
@@ -98,7 +98,21 @@ app.post('/api/persons', (req, res) => {
   }
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+   .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+   .catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
