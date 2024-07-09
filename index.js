b8acaca5ba658ad89e59f4fc860da419c474a8d2
tomi <tomislav.varga@icloud.com>
2024-07-09 09:53:45 +0200

    solution for exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,106 +1,90 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
+require('dotenv').config()
+
+const Person = require('./models/person')
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
 ]
 
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(cors())
 app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(person => person.id === id)
+  Person.findById(req.params.id).then(person => {
     if (person) {
       res.json(person)
     } else {
       res.status(404).end()
     }
+  }).catch(error => {
+    res.status(500).send({ error: 'malfunctioning server' })
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id!== id)
-    res.status(204).end()
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
-const generateId = () => {
-    return String(Math.floor(Math.random() * 1000000))
-}
-
-app.post('/api/persons/', (req, res) => {
-    const body = req.body
-    const personExists = persons.some(person => person.name === body.name)
-    if (personExists) {
-      res.status(400).json({ error: 'name must be unique' })
-    } else {
-      
-
-      if (!body.name) {
-        return res.status(400).json({ 
-          error: 'name missing' 
-        })
-      }
-
-      if (!body.number) {
-        return res.status(400).json({ 
-          error: 'number missing' 
-        })
-      }
-
-      const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number, 
-      }
-
-      persons = persons.concat(person)
-
-      res.json(person)
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'content missing' })
+  } else {
+    if (!body.name) {
+      return res.status(400).json({ error: 'name missing' })
     }
+    if (!body.number) {
+      return res.status(400).json({ error: 'number missing' })
+    }
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
+  }
 })
 
-const PORT = process.env.PORT || 3002
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
